966415d80a621be9b9e90b12b86e0f14725f3e0d
suncy <3301065990@qq.com>
2019-12-09 16:35:03 +0800

    增加emqx_acl_custom

---
+++ src/emqx_acl_custom.erl
@@ -0,0 +1,36 @@
+%% Copyright (c) 2018 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(emqx_acl_custom).
+
+-include_lib("emqx/include/emqx.hrl").
+
+%% ACL callbacks
+-export([ init/1
+        , check_acl/5
+        , reload_acl/1
+        , description/0
+        ]).
+
+init(Opts) ->
+    {ok, Opts}.
+
+check_acl({Credentials, PubSub, _NoMatchAction, Topic}, _State) ->
+    io:format("ACL Demo: ~p ~p ~p~n", [Credentials, PubSub, Topic]),
+    allow.
+
+reload_acl(_State) ->
+    ok.
+
+description() -> "ACL Custom Module".
